a56abb00850d5c7e52cd1615643d4fb1a0cea643
ivan <398778994@qq.com>
2017-11-25 00:15:12 +0800

    返回列表保持位置

---
+++ vue-sku/src/components/detail2list/list.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="detail2List">
+    <ul>
+      <li v-for="i in 1000" :key="i"><router-link :to="{name: 'detail'}">{{i}}</router-link></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'detail2List',
+  props: {
+  },
+  watch: {
+  },
+  data () {
+    return {
+    }
+  },
+  mounted () {
+    console.log('mounted')
+  },
+  activated () {
+    console.log('activated')
+  },
+  methods: {
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  * {
+    margin: 0;
+    padding: 0;
+  }
+</style>
--- vue-sku/src/router/index.js
+++ vue-sku/src/router/index.js
@@ -1,15 +1,44 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import detail2List from '@/components/detail2list/list'
+import detail2Listdetail from '@/components/detail2list/detail'
 
 Vue.use(Router)
 
 export default new Router({
+  // mode: 'hash', // default
+  mode: 'history', // 需要服务器支持，否者刷新404
   routes: [
     {
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/detail2List',
+      name: 'detail2List',
+      component: detail2List,
+      meta: {
+        keepAlive: true
+      }
+    },
+    {
+      path: '/detail',
+      name: 'detail',
+      component: detail2Listdetail
     }
-  ]
+  ],
+  scrollBehavior (to, from, savedPosition) {
+    // scrollBehavior只在history模式下可用
+    console.log(savedPosition)
+    // if (savedPosition) {
+    //   return savedPosition
+    // } else {
+    //   if (from.meta.keepAlive) {
+    //     from.meta.savedPosition = document.body.scrollTop
+    //   }
+    //   return { x: 0, y: to.meta.savedPosition || 0 }
+    // }
+  }
 })
